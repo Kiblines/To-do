e8be2b78c41227c2d1d17f7f4e125880e73d8c8f
Anthony Lines <linesanthony018@gmail.com>
2023-06-26 14:44:54 +0200

    CLEAN

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,9 +1,20 @@
 //  Ce composant sera responsable de l'affichage de la liste des tâches. 
 //Il itérera sur les données des tâches et rendra un composant TaskItem pour chaque tâche.
 
+import styled from 'styled-components';
+
+const TaskListContainer = styled.div `
+display: flex;
+justify-content:center;
+
+`;
+
+
 
 export default function TaskList() {
   return (
-    <div>TaskList</div>
+    <TaskListContainer>
+
+    </TaskListContainer>
   )
 }
