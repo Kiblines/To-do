ee630895f8fe7ba4bbe95d989dbe1733bb516537
Anthony Lines <linesanthony018@gmail.com>
2023-06-28 20:19:40 +0200

    Task item props

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+import { Task } from "../types/task";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
 // TaskItem : Ce composant représentera une tâche individuelle dans la liste.
 //Il affichera les détails de la tâche, tels que le titre, la description, la date d'échéance, etc.
 //Il peut également inclure des boutons d'édition et de suppression pour interagir avec la tâche.
+type TaskProps = {
+  task: Task;
+  onDeleteTask: (task: Task) => void;
+  onEditTask: (task: Task) => void;
+};
 
-export default function TaskItem() {
-  return <div>TaskItem</div>;
+function TaskItem(props: TaskProps) {
+  const { task, onDeleteTask, onEditTask } = props;
+  const [isEditing, setIsEditing] = useState(false);
+  const [taskName, setTaskName] = useState(task.name);
+
+  return <Container></Container>;
 }
+
+export default TaskItem;
