daec5380b290ccaa26e3cecdc0ae6f9f1fcdcd84
Anthony Lines <linesanthony018@gmail.com>
2023-06-28 18:00:04 +0200

    Change & Submit added

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,10 +1,16 @@
 import styled from "styled-components";
-import Task from "../types/task";
+import { Task } from "../types/task";
 import useNewId from "../hooks/useNewId";
+import { useState } from "react";
 
 //TaskForm : Ce composant permettra à l'utilisateur de créer une nouvelle tâche en saisissant les détails requis.
 //Il affichera un formulaire avec des champs tels que le titre, la description, la date d'échéance, etc.
 //Lorsque le formulaire est soumis, il déclenchera une fonction pour ajouter la nouvelle tâche à la liste.
+
+const FormContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 type TaskFormProps = {
   onCreateTask: (task: Task) => void;
@@ -15,7 +21,24 @@ function TaskForm(props: TaskFormProps) {
   const getNewId = useNewId(firstId);
   const [task, setTask] = useState({ id: firstId, name: "" });
 
-  const handleTaskChange = (e:)
+  const handleTaskChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setTask({ ...task, name: value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    props.onCreateTask({ ...task, id: getNewId(), isCompleted: false });
+  };
+
+  return (
+    <FormContainer>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleTaskChange} />
+        <button type="submit">追加</button>
+      </form>
+    </FormContainer>
+  );
 }
 
 export default TaskForm;
