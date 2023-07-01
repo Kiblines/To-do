a9fe6d7b460d08c2e368f0a5e9c948509e387c71
Anthony Lines <linesanthony018@gmail.com>
2023-07-01 20:02:46 +0200

    style(Form): :lipstick:

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Task } from "../types/task";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const ItemContainer = styled.div``;
 
 // TaskItem : Ce composant représentera une tâche individuelle dans la liste.
 //Il affichera les détails de la tâche, tels que le titre, la description, la date d'échéance, etc.
@@ -11,14 +11,23 @@ type TaskProps = {
   task: Task;
   onDeleteTask: (task: Task) => void;
   onEditTask: (task: Task) => void;
+  onCompleteTask: (task: Task) => void;
 };
 
 function TaskItem(props: TaskProps) {
-  const { task, onDeleteTask, onEditTask } = props;
+  const { task, onDeleteTask, onEditTask, onCompleteTask } = props;
   const [isEditing, setIsEditing] = useState(false);
   const [taskName, setTaskName] = useState(task.name);
 
-  return <Container></Container>;
+  return (
+    <ItemContainer>
+      <input
+        type="checkbox"
+        onChange={() => onCompleteTask(task)}
+        checked={task.isCompleted}
+      />
+    </ItemContainer>
+  );
 }
 
 export default TaskItem;
