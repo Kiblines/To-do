c75743ee47b12aa1fc647acb665797ea8dfb9980
Anthony Lines <linesanthony018@gmail.com>
2023-07-06 14:34:29 +0200

    import list

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,7 +1,11 @@
 //  Ce composant sera responsable de l'affichage de la liste des tâches.
 //Il itérera sur les données des tâches et rendra un composant TaskItem pour chaque tâche.
+import TaskItem from "./TaskItem";
 
 import styled from "styled-components";
+import { Task } from "../types/task"
+
+
 
 const TaskListContainer = styled.div`
   display: flex;
